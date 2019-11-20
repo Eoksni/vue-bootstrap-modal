877448690efbf7d75a28eb59dad4141e29574c6b
Dmitry Mazurok <mazurok@outlook.com>
2019-11-20 20:07:05 +0700

    fix #6 - add ESC for function-style

--- src/modal.js
+++ src/modal.js
@@ -5,23 +5,27 @@ export default {
       required: true
     }
   },
-  mounted: function() {
-    document.addEventListener('keydown', function(e) {
+  mounted: function () {
+    document.addEventListener('keydown', this.onEscape);
+  },
+  unmounted: function () {
+    document.removeEventListener('keydown', this.onEscape);
+  },
+  methods: {
+    onEscape: function (e) {
       if (this.show && e.keyCode === 27) {
         this.cancel();
       }
-    });
-  },
-  methods: {
-    close: function() {
+    },
+    close: function () {
       // this is Vue's fancy way of doing `this.show = false`
       this.$emit('update:show', false);
     },
-    cancel: function() {
+    cancel: function () {
       this.close();
       this.$emit('cancel');
     },
-    ok: function() {
+    ok: function () {
       this.close();
       this.$emit('ok');
     }
